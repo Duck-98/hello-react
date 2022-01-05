47aaa2e41f1c0a5a581724c9c157882cd0ef38b0
duck <godqhr2256@gmail.com>
2022-01-05 22:27:33 +0900

    9장 실습 및 내용정리 ~ing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
-import Average from './Chapter 8/Average';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
-  return <Average />;
-};
-
+class App extends Component {
+  render() {
+    return (
+      <div className='="App-header'>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="http://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
 /*import LifeCycleSample from './LifeCycleSample';
 import ErrorBoundary from './ErrorBoundary';
 
