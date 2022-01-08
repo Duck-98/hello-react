5282b7a8fe8d49e9fb9ff8bee8e0a92950ce375d
duck <godqhr2256@gmail.com>
2022-01-08 23:16:11 +0900

    10장 일정추가 오류 수정

--- src/Chapter 10/components/TodoInsert.js
+++ src/Chapter 10/components/TodoInsert.js
@@ -18,13 +18,13 @@ const TodoInsert = ({ onInsert }) => {
   );
 
   return (
-    <form className="TodoInsert">
+    <form className="TodoInsert" onSubmit={onSubmit}>
       <input
         placeholder="할 일을 입력해주세요"
         value={value}
         onChange={onChange}
       />
-      <button type="submit" onSubmit={onSubmit}>
+      <button type="submit">
         <MdAdd />
       </button>
     </form>
